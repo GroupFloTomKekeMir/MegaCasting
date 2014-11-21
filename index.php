ea11c57d716353e2a698238f0c58423ef66691d5
KHARDY <KHARDY@B15-14.cfcci53.fr>
2014-11-21 16:20:30 +0100

    Ajout de Test

--- index.php
+++ index.php
@@ -11,8 +11,38 @@ and open the template in the editor.
         <title>MegaCasting</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+       <header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="./" class="navbar-brand">MegaCasting</a>
+    </div>
+    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+      <ul class="nav navbar-nav"> 
+        <li>
+          <a href="#">Musiques</a>
+        </li>
+        <li>
+          <a href="#">Boutique</a>
+        </li>
+        <li class="active">
+          <a href="#">Spectacles</a>
+        </li>
+        <li class="active">
+          <a href="#">Castings</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+        
+        <footer class="row">
+            
+        </footer>
     </body>
 </html>
