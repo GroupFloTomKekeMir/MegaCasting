933f563f5bfddb661d52723c9332a983da53c19a
MegaProjet <hardy.k@neuf.fr>
2014-11-23 18:10:53 +0100

    ajout de la page de connexion

---
+++ Connexion.php
@@ -0,0 +1,82 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <title>MegaCasting</title>
+    </head>
+    <body>
+       <header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="./" class="navbar-brand">MegaCasting</a>
+    </div>
+    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+      <ul class="nav navbar-nav"> 
+        <li>
+          <a href="#">Musiques</a>
+        </li>
+        <li>
+          <a href="#">Boutique</a>
+        </li>
+        <li class="active">
+          <a href="#">Spectacles</a>
+        </li>
+        <li class="active">
+          <a href="#">Castings</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+<!----------------------------------------------------------------------------------->
+
+<div class="contain" style="margin-top : 60px;">
+
+<div class="row">
+<div class="span2 offset10">
+
+<h1> Connexion </h1>
+
+
+
+<form action="demo_form.asp">
+
+<div class="col-md-3 col-lg-1">
+<label for = "identifiant">Pseudo : </label>
+<input name="identifiant" id="identifiant" type="text" />
+</div>
+<div>
+<label for ="password"> Mot de passe : </label>
+<input name ="pasword" id="password" type="password" />
+</div>
+
+<br/>
+<div>
+      <input id="submit" type ="submit" value ="Connexion" style="text-align : center" />
+</div>
+
+</form>
+
+
+</div>
+	</div>
+</div>
+
+
+
+
+
+
+<!----------------------------------------------------------------------------------->
+     
+        <footer class="row">
+            
+        </footer>
+    </body>
+</html>
